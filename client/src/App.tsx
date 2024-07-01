8afa86a7b20b00ba8b747bd91202d327f30915d8
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-01 23:37:30 +0200

    shadcn install

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,10 @@
-import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./pages/nav/Navbar";
-import Footer from "./pages/footer/Footer";
-import ErrorPage from "./pages/error/ErrorPage";
-import LoadingSpinner from "./pages/loading/LoadingSpinner";
-
-const MainPage = lazy(() => import("./pages/main/MainPage"));
-const Gallery = lazy(() => import("./pages/gallery/Gallery"));
-const Certificates = lazy(() => import("./pages/certificate/Certificates"));
+import { Button } from "@/components/ui/button";
 
 const App = () => {
   return (
-    <Suspense fallback={<LoadingSpinner />}>
-      <Router>
-        <Navbar />
-
-        <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/gallery" element={<Gallery />}></Route>
-          <Route path="/certificates" element={<Certificates />}></Route>
-          <Route path="*" element={<ErrorPage />} />
-        </Routes>
-
-        <Footer />
-      </Router>
-    </Suspense>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 };
 
--- client/src/pages/certificate/Certificates.tsx
+++
@@ -1,9 +0,0 @@
-const Certificates = () => {
-  return (
-    <div>
-      <h1>Certificates</h1>
-    </div>
-  );
-};
-
-export default Certificates;
