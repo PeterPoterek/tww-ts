063fa20ce874f96103119fab9d9ef44451173fa7
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-01 23:45:31 +0200

    jsx reimported

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,31 @@
-import { Button } from "@/components/ui/button";
+import { Suspense, lazy } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./pages/nav/Navbar";
+import Footer from "./pages/footer/Footer";
+import ErrorPage from "./pages/error/ErrorPage";
+import LoadingSpinner from "./pages/loading/LoadingSpinner";
+
+const MainPage = lazy(() => import("./pages/main/MainPage"));
+const Gallery = lazy(() => import("./pages/gallery/Gallery"));
+const Certificates = lazy(() => import("./pages/certificate/Certificates"));
 
 const App = () => {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="/gallery" element={<Gallery />}></Route>
+          <Route path="/certificates" element={<Certificates />}></Route>
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+    </Suspense>
   );
 };
 
---
+++ client/src/pages/nav/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { Button } from "@/components/ui/button";
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar
+      <div>
+        <Link to="/">Main</Link>
+        <Link to="/gallery">Gallery</Link>
+        <Link to="/certificates">Certificates</Link>
+        <Button>Click me</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
