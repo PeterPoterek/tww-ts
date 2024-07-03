8bcb5c10afa092bdf936a9c1f067eb0f51bc69fd
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-03 23:18:22 +0200

    open/close modal animation

--- client/src/pages/nav/NavbarMobileModal.tsx
+++ client/src/pages/nav/NavbarMobileModal.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import { RiCloseLargeFill } from "react-icons/ri";
+import { motion } from "framer-motion";
 
 interface NavbarMobileModalProps {
   isModalOpen: boolean;
@@ -7,56 +8,72 @@ interface NavbarMobileModalProps {
 }
 
 const NavbarMobileModal = ({ isModalOpen, setIsModalOpen }: NavbarMobileModalProps) => {
-  return (
-    isModalOpen && (
-      <>
-        <div className="fixed z-50 inset-0 bg-background flex justify-center items-center">
-          <div className="absolute top-0 right-0 ">
-            <RiCloseLargeFill
-              className="w-[35px] h-[35px] mt-10 mr-10"
-              onClick={() => {
-                setIsModalOpen(false);
-              }}
-            />
-          </div>
+  const modalVariants = {
+    hidden: {
+      x: "100%",
+    },
+    visible: {
+      x: "0%",
+      transition: {
+        type: "spring",
+        damping: 20,
+        stiffness: 100,
+        duration: 0.5,
+      },
+    },
+  };
 
-          <div
-            className="p-5"
-            onClick={() => {
-              setIsModalOpen(false);
-            }}
-          >
-            <ul className="flex flex-col gap-[3rem] justify-center items-center">
-              <li>
-                <Link to={"/#about"} className="text-xl font-bold">
-                  O nas
-                </Link>
-              </li>
-              <li>
-                <Link to={"/#reviews"} className="text-xl font-bold">
-                  Opinie
-                </Link>
-              </li>
-              <li>
-                <Link to={"/#contact"} className="text-xl font-bold">
-                  Kontakt
-                </Link>
-              </li>
-              <li>
-                <Link to={"/gallery"} className="text-xl font-bold">
-                  Galeria
-                </Link>
-              </li>
-              <li>
-                <Link to={"/certificates"} className="text-xl font-bold">
-                  Certyfikaty
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </>
-    )
+  return (
+    <motion.div
+      className="fixed z-50 inset-0 bg-background flex justify-center items-center"
+      initial="hidden"
+      animate={isModalOpen ? "visible" : "hidden"}
+      variants={modalVariants}
+    >
+      <div className="absolute top-0 right-0">
+        <RiCloseLargeFill
+          className="w-[35px] h-[35px] mt-10 mr-10 cursor-pointer"
+          onClick={() => {
+            setIsModalOpen(false);
+          }}
+        />
+      </div>
+
+      <div
+        className="p-5"
+        onClick={() => {
+          setIsModalOpen(false);
+        }}
+      >
+        <ul className="flex flex-col gap-[3rem] justify-center items-center">
+          <li>
+            <Link to={"/#about"} className="text-xl font-bold">
+              O nas
+            </Link>
+          </li>
+          <li>
+            <Link to={"/#reviews"} className="text-xl font-bold">
+              Opinie
+            </Link>
+          </li>
+          <li>
+            <Link to={"/#contact"} className="text-xl font-bold">
+              Kontakt
+            </Link>
+          </li>
+          <li>
+            <Link to={"/gallery"} className="text-xl font-bold">
+              Galeria
+            </Link>
+          </li>
+          <li>
+            <Link to={"/certificates"} className="text-xl font-bold">
+              Certyfikaty
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </motion.div>
   );
 };
 
