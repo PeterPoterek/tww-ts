609241fb5ac89d85d819105559322e926611645c
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-03 21:52:55 +0200

    Update NavbarLink.tsx

--- client/src/pages/nav/NavbarLink.tsx
+++ client/src/pages/nav/NavbarLink.tsx
@@ -5,10 +5,10 @@ interface NavbarLinkProps {
   text: string;
 }
 
-const NavbarLink = (props: NavbarLinkProps) => {
+const NavbarLink = ({ path, text }: NavbarLinkProps) => {
   return (
     <li className="relative overflow-hidden border-b-2 border-transparent transition duration-300 after:absolute after:bottom-0 after:right-full after:h-[2.5px] after:w-full after:bg-zinc-900 after:duration-300 hover:after:translate-x-full text-lg">
-      <Link to={props.path}>{props.text}</Link>
+      <Link to={path}>{text}</Link>
     </li>
   );
 };
