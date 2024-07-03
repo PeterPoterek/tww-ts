309778f89e093be74d6c4931bbbc02dfdea9d4d1
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-03 22:10:10 +0200

    npm i react-icons

--- client/src/pages/nav/Navbar.tsx
+++ client/src/pages/nav/Navbar.tsx
@@ -2,15 +2,16 @@ import phoneIcon from "../../assets/phone-icon.svg";
 import ScrollToHashElement from "@cascadia-code/scroll-to-hash-element";
 import NavbarLink from "./NavbarLink";
 import NavbarLogoLink from "./NavbarLogoLink";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
   return (
     <div className="fixed w-full z-50 bg-background rounded shadow-xl">
       <ScrollToHashElement behavior="smooth" inline="center" block="start" />
-      <div className="max-w-7xl mx-auto flex justify-between items-center p-3">
+      <div className="xl:max-w-7xl mx-auto flex justify-between items-center p-3">
         <NavbarLogoLink />
 
-        <menu className="flex gap-5 ">
+        <menu className="hidden xl:flex gap-5 ">
           <NavbarLink path={"/#about"} text={"O nas"} />
           <NavbarLink path={"/#reviews"} text={"Opinie"} />
           <NavbarLink path={"/#contact"} text={"Kontakt"} />
@@ -18,9 +19,13 @@ const Navbar = () => {
           <NavbarLink path={"/certificates"} text={"Certyfikaty"} />
         </menu>
 
-        <div className="flex gap-3 justify-center items-center">
+        <div className="hidden xl:flex gap-3 justify-center items-center">
           <img src={phoneIcon} alt="Phone Icon" width="30" height="30" />
           <span className="text-xl font-bold">696-075-595</span>
+        </div>
+
+        <div className="xl:hidden">
+          <GiHamburgerMenu className="w-[40px] h-[40px] " />
         </div>
       </div>
     </div>
