3b60f4d08ebb4c3cf81558ff90b99c3bfe6b9046
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-10 19:18:39 +0200

    Services adjusted

--- client/src/pages/main/ServicesSection/ServicesList.tsx
+++ client/src/pages/main/ServicesSection/ServicesList.tsx
@@ -2,14 +2,18 @@ interface ServicesListProps {
   label: string;
   services: string[];
 }
+import { Separator } from "@/components/ui/separator";
 
 const ServicesList = ({ label, services }: ServicesListProps) => {
   return (
-    <div className="w-[220px] sm:w-[270px] h-[180px] flex flex-col bg-neutral-200 p-5 rounded-md gap-2">
-      <h3 className="font-bold">{label}:</h3>
+    <div className="w-[220px] sm:w-[270px] h-[180px] flex flex-col bg-slate-900 p-5 rounded-md gap-2">
+      <h3 className="text-slate-200  font-bold">{label}:</h3>
+
+      <Separator />
+
       <ul className="flex flex-col gap-1">
         {services.map((service) => {
-          return <li className="">- {service}</li>;
+          return <li className="text-slate-200 ">- {service}</li>;
         })}
       </ul>
     </div>
