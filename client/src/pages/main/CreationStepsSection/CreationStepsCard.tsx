61c224e23933c0e6ae5f5b9de6e2f39f21b6cd97
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-11 20:00:28 +0200

    Update CreationStepsCard.tsx

--- client/src/pages/main/CreationStepsSection/CreationStepsCard.tsx
+++ client/src/pages/main/CreationStepsSection/CreationStepsCard.tsx
@@ -5,10 +5,10 @@ interface CreationStepsCardProps {
 
 const CreationStepsCard = ({ src, cardText }: CreationStepsCardProps) => {
   return (
-    <div className="flex flex-col justify-center items-center  w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+    <div className="w-[220px] flex flex-col justify-center items-center sm:w-1/2 md:w-1/3 lg:w-1/4">
       <img src={src} alt="Stock image" className="w-full" />
 
-      <p className="rounded-b-md p-5 w-full bg-slate-900 text-slate-200 text-center tracking-wider font-bold">{cardText}</p>
+      <p className="text-sm md:text-base rounded-b-md p-5 w-full bg-slate-900 text-slate-200 text-center tracking-wider font-bold">{cardText}</p>
     </div>
   );
 };
