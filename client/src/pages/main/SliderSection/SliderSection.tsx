991476ae977604138b6c0a3732a9a38f7ffd4123
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-12 23:44:36 +0200

    minor adjusements

--- client/src/pages/main/SliderSection/SliderSection.tsx
+++ client/src/pages/main/SliderSection/SliderSection.tsx
@@ -1,9 +1,7 @@
 import { useNavigate } from "react-router-dom";
-
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
 import gallery1 from "../../../assets/gallery1.png";
 import gallery2 from "../../../assets/gallery2.png";
 import gallery3 from "../../../assets/gallery3.png";
@@ -12,6 +10,7 @@ import gallery5 from "../../../assets/gallery5.png";
 
 const SliderSection = () => {
   const navigate = useNavigate();
+
   const settings = {
     dots: false,
     arrows: false,
@@ -23,7 +22,13 @@ const SliderSection = () => {
       {
         breakpoint: 768,
         settings: {
-          slidesToShow: 1,
+          slidesToShow: 3,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
         },
       },
     ],
