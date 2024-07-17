7cd342041e3fdeb234ec0dc97adbb1ec0ceec98e
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-17 19:53:26 +0200

    npm i @react-google-maps/api

--- client/src/pages/main/ContactSection/ContactSection.tsx
+++ client/src/pages/main/ContactSection/ContactSection.tsx
@@ -1,7 +1,11 @@
 import Section from "@/pages/shared/Section";
 
 const ContactSection = () => {
-  return <Section id="contact">ContactSection</Section>;
+  return (
+    <Section id="contact">
+      <h2 className="text-center  text-2xl md:text-2xl xl:text-3xl font-bold">Kontakt</h2>
+    </Section>
+  );
 };
 
 export default ContactSection;
