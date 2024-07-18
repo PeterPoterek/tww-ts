5b568196132722910ddb7d9dba5c4a62b6634b61
Piotr Poterek <peterpoterek0@gmail.com>
2024-07-18 20:58:30 +0200

    Update ContactSection.tsx

--- client/src/pages/main/ContactSection/ContactSection.tsx
+++ client/src/pages/main/ContactSection/ContactSection.tsx
@@ -2,7 +2,6 @@ import Section from "@/pages/shared/Section";
 
 const ContactSection = () => {
   return (
-    //
     <Section id="contact">
       <h2 className="text-center  text-2xl md:text-2xl xl:text-3xl font-bold">Kontakt</h2>
     </Section>
